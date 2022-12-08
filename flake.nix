f105fc620a80cd3fad22af2443279fd08d259e24
Gabriel Fontes <hi@m7.rs>
2022-12-08 03:11:02 -0300

    ajeitar flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,7 @@
 {
-  description = "Smart2get website";
-
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-  };
-
-  outputs = { self, nixpkgs, nix-colors }:
+  description = "Yrmos";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+  outputs = { self, nixpkgs }:
     let
       forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
       pkgsFor = nixpkgs.legacyPackages;
