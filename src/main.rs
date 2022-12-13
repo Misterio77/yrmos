a61e94975b18ad08928b42d1d7c8e5a4d768e28e
Gabriel Fontes <hi@m7.rs>
2022-12-13 18:17:16 -0300

    redirecionar home p/ rides

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use axum::{routing::get, Router, Server};
+use axum::{response::Redirect, routing::get, Router, Server};
 use clap::Parser;
 
-use maud::{html, Markup};
+use maud::Markup;
 use yrmos::{
     common::{config::AppConfig, errors::AppError, style},
     layouts,
@@ -9,8 +9,8 @@ use yrmos::{
     schema::Session,
 };
 
-async fn home(session: Option<Session>) -> Markup {
-    layouts::default(html! {}, session.as_ref())
+async fn home() -> Redirect {
+    Redirect::to("/rides")
 }
 async fn fallback(session: Option<Session>) -> Markup {
     layouts::default(AppError::NotFound.as_html(), session.as_ref())
