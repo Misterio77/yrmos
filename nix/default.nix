cb28366f369a2331e97f96ff0c4c61ee4bcaa543
Gabriel Fontes <hi@m7.rs>
2023-05-03 14:20:16 -0300

    use sourceByRegex to filter src

--- nix/default.nix
+++ nix/default.nix
@@ -1,38 +1,29 @@
-{ pkgs ? import <nixpkgs> { }, ... }:
+{ lib, rustPlatform, fetchFromGitHub, ... }:
 
 let
-  manifest = (pkgs.lib.importTOML ../Cargo.toml).package;
+  manifest = (lib.importTOML ../Cargo.toml).package;
   # Flake does not support submodule (https://github.com/NixOS/nix/issues/4423)
   # Fetch manually for now
-  picocss = pkgs.fetchFromGitHub {
+  picocss = fetchFromGitHub {
     owner = "picocss";
     repo = "pico";
     rev = "v1.4.4";
     sha256 = "sha256-k3ovaAa/mC+jO9rgyOZAq8FfwWJkK3uypwxZ6NXvFEo=";
   };
-  excludeBaseNames = files: (path: _: builtins.all (x: (builtins.baseNameOf path) != x) files);
 in
-pkgs.rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage {
   pname = manifest.name;
   version = manifest.version;
 
-  src = builtins.path {
-    name = "yrmos-source";
-    path = ../.;
-    filter = (excludeBaseNames [
-      "result"
-      "target"
-      "nixos.qcow"
-      "nix"
-      "flake.nix"
-      "flake.lock"
-      "README.md"
-      "LICENSE"
-      ".gitattributes"
-      ".gitmodules"
-      ".gitignore"
-    ]);
-  };
+  src =  lib.sourceByRegex ../. [
+    "Cargo.toml"
+    "Cargo.lock"
+    "sqlx-data.json"
+    "build.rs"
+    "assets.*"
+    "db.*"
+    "src.*"
+  ];
 
   cargoLock = {
     lockFile = ../Cargo.lock;
@@ -47,7 +38,7 @@ pkgs.rustPlatform.buildRustPackage rec {
     ln ${picocss} -sfT assets/scss/pico
   '';
 
-  meta = with pkgs.lib; {
+  meta = with lib; {
     description = manifest.description;
     homepage = manifest.homepage;
     license = licenses.mit;
