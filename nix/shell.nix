0f0d3efe6a0958475dc36e600a4dad92d1c76b11
Gabriel Fontes <hi@m7.rs>
2023-05-05 15:02:13 -0300

    fazer um motd bonitinho

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,29 +1,31 @@
 { pkgs ? import <nixpkgs> { }, ... }:
 
 let
-  db-script = pkgs.writeShellScriptBin "db" ''
-    set -e
-    operation="''${1:-none}"
-    if [ "$operation" == "up" ]; then
-      shift
-      mkdir -p "$PGDATA" "$PGHOST"
-      pg_ctl --silent init
-      pg_ctl --silent start -l "$PGLOG" -o "-k $PGHOST"
-      psql -c 'CREATE DATABASE yrmos' postgres
-      echo "Database created"
-      touch "src/main.rs" # Make server reload if watching
-    elif [ "$operation" == "down" ]; then
-      shift
-      pg_ctl --silent status && pg_ctl --silent stop || true
-      rm "$PGDATA" "$PGHOST" -rf
-      echo "Database destroyed"
-    elif [ "$operation" == "logs" ]; then
-      less "$PGLOG"
-    elif [ "$operation" == "tail" ]; then
-      tail "$PGLOG"
-    else
-      exec -a db pg_ctl "$@"
-    fi
+  motd = pkgs.writeShellScriptBin "motd" ''
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    YELLOW='\033[0;33m'
+    BLUE='\033[0;34m'
+    PURPLE='\033[0;35m'
+    CYAN='\033[0;36m'  
+    NC='\033[0m'
+
+    echo -e $YELLOW"Sua shell e a aplicação estão configuradas para um postgres local."$NC
+    echo -e ""
+    echo -e "Inicializar o postgres:             " $GREEN"pg_ctl init"$NC
+    echo -e "Executar o postgres:                " $GREEN"pg_ctl start -o \"-k \$PGSOCKET\""$NC
+    echo -e "Status do postgres:                 " $CYAN"pg_ctl status"$NC
+    echo -e "Parar o postgres:                   " $RED"pg_ctl stop"$NC
+    echo -e ""
+    echo -e "Criar a base:                       " $GREEN"psql postgres -c \"CREATE DATABASE $PGDATABASE\""$NC
+    echo -e "Conectar na base:                   " $CYAN"psql"$NC
+    echo -e "Apagar a base:                      " $RED"psql -c \"DROP DATABASE $PGDATABASE\""$NC
+    echo -e ""
+    echo -e "Compilar aplicação:                 " $GREEN"cargo build"$NC
+    echo -e "Rodar aplicação:                    " $GREEN"cargo run"$NC
+    echo -e "Recompilar e rodar automaticamente: " $YELLOW"cargo watch -x run"$NC
+    echo -e ""
+    echo -e "Ver essa mensagem novamente:        " $PURPLE"motd"$NC
   '';
 in
 (pkgs.callPackage ./default.nix { }).overrideAttrs (oa: {
@@ -40,28 +42,26 @@ in
     pgformatter
     sqls
     db
-    # SCSS tooling
-    sass
-    # Custom DB script wrapper
-    db-script
+    # Message of the day
+    motd
   ] ++ (oa.nativeBuildInputs or []);
 
   # Random, but stable secret key while developing
   # To avoid having to log in again all the time
   shellHook = ''
+      mkdir -p "$(pwd)/.db"
       export PGDATA="$(pwd)/.db/data"
-      export PGHOST="$(pwd)/.db/socket"
-      export PGLOG="$(pwd)/.db/log"
+      export PGSOCKET="$(pwd)/.db/socket"
+      mkdir -p $PGSOCKET
 
       export PGPORT="5430"
       export PGDATABASE="yrmos"
       export PGUSER="$USER"
+      export PGHOST="$PGSOCKET"
 
       export YRMOS_DATABASE="postgres://$PGUSER/$PGDATABASE?host=$PGHOST&port=$PGPORT"
       export YRMOS_SECRET_KEY=$(${pkgs.libressl}/bin/openssl rand -base64 64)
 
-      echo "Sua shell está configurada para ter uma base local de postgres."
-      echo "Use o comando 'db up' para criar e 'db down' para destruir."
-      echo "Use 'cargo watch -x run' para executar a aplicação com reloads."
+      motd
   '';
 })
