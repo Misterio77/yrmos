31c639c1704bfec560fe5fc4b443fce4764eb181
Gabriel Fontes <hi@m7.rs>
2023-05-05 12:37:31 -0300

    possibilitar rodar um postgres local facinho

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,5 +1,31 @@
 { pkgs ? import <nixpkgs> { }, ... }:
 
+let
+  db-script = pkgs.writeShellScriptBin "db" ''
+    set -e
+    operation="''${1:-none}"
+    if [ "$operation" == "up" ]; then
+      shift
+      mkdir -p "$PGDATA" "$PGHOST"
+      pg_ctl --silent init
+      pg_ctl --silent start -l "$PGLOG" -o "-k $PGHOST"
+      psql -c 'CREATE DATABASE yrmos' postgres
+      echo "Database created"
+      touch "src/main.rs" # Make server reload if watching
+    elif [ "$operation" == "down" ]; then
+      shift
+      pg_ctl --silent status && pg_ctl --silent stop || true
+      rm "$PGDATA" "$PGHOST" -rf
+      echo "Database destroyed"
+    elif [ "$operation" == "logs" ]; then
+      less "$PGLOG"
+    elif [ "$operation" == "tail" ]; then
+      tail "$PGLOG"
+    else
+      exec -a db pg_ctl "$@"
+    fi
+  '';
+in
 (pkgs.callPackage ./default.nix { }).overrideAttrs (oa: {
   nativeBuildInputs = with pkgs; [
     # Rust tooling
@@ -13,14 +39,29 @@
     postgresql
     pgformatter
     sqls
+    db
     # SCSS tooling
     sass
-  ] ++ oa.nativeBuildInputs;
+    # Custom DB script wrapper
+    db-script
+  ] ++ (oa.nativeBuildInputs or []);
 
   # Random, but stable secret key while developing
   # To avoid having to log in again all the time
   shellHook = ''
-      export YRMOS_SECRET_KEY="$(${pkgs.libressl}/bin/openssl rand -base64 64)"
-      echo "Use 'cargo watch -x run' para executar com reloads automáticos."
+      export PGDATA="$(pwd)/.db/data"
+      export PGHOST="$(pwd)/.db/socket"
+      export PGLOG="$(pwd)/.db/log"
+
+      export PGPORT="5430"
+      export PGDATABASE="yrmos"
+      export PGUSER="$USER"
+
+      export YRMOS_DATABASE="postgres://$PGUSER/$PGDATABASE?host=$PGHOST&port=$PGPORT"
+      export YRMOS_SECRET_KEY=$(${pkgs.libressl}/bin/openssl rand -base64 64)
+
+      echo "Sua shell está configurada para ter uma base local de postgres."
+      echo "Use o comando 'db up' para criar e 'db down' para destruir."
+      echo "Use 'cargo watch -x run' para executar a aplicação com reloads."
   '';
 })
